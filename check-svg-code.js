bd123e5fb5a166a104df9a6a64b7d054657ed969
David Melsheimer <dave.melshman@gmail.com>
2021-11-15 13:15:19 -0500

    having a hell of a time getting the generated elements to display when appended

--- check-svg-code.js
+++ check-svg-code.js
@@ -1,4 +1,16 @@
 //Color swatch array generation:
+
+
+function hashToColorArray(inputHash){
+    let colorArray = [];
+    let i = 0;
+    while(i < 60){
+        colorArray.push(`#${inputHash.slice(i, i + 6)}`);
+        i += 6;
+    }
+    return colorArray;
+}
+
 const componentToHex =function (c){
     var hex = c.toString(16);
     return hex.length == 1 ? '0' + hex : hex;
@@ -8,18 +20,21 @@ function rgtToHex(r, g, b){
     return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
 }
 
-const hashFunc = function(){
-    var hash = 0;
-    if (this.length === 0) return hash;
-    for(i=0;i<this.length;i++){
-        char = this.charCodeAt(i);
-        hash = ((hash<<5)-hash)+char;
-        hash = hash & hash;
+const colorArray = custInput => hashToColorArray(sha256(custInput));
+
+function makeColorSwatch(inputArray){
+    var swatchNode = document.getElementsByClassName('color-swatch')[0];
+    //let swatchCode = document.getElementsByClassName('color-swatch')[0];
+    for(let i=0; i<inputArray.length;i++){
+        let newNode = document.createElement('div');
+        newNode.setAttribute('width', '50px');
+        newNode.setAttribute('height', '50px');
+        newNode.setAttribute('border', 'solid black');
+        newNode.setAttribute('background', inputArray[i]);
+        swatchNode.appendChild(newNode);
     }
-    return hash;
+    console.log(swatchNode);
 }
-
-
 
 //begin control of head/stroke slider (called Slider 1);
 const headColorSlider = document.getElementById('head_color_slider');
@@ -50,3 +65,107 @@ headColorSlider.addEventListener("change", function() {
 
 
 //Slider 2, body
+
+
+
+
+
+//sha256 implementation in JS for generating color sets
+//https://geraintluff.github.io/sha256/
+const sha256 = (ascii) => {
+	function rightRotate(value, amount) {
+		return (value>>>amount) | (value<<(32 - amount));
+	};
+	
+	var mathPow = Math.pow;
+	var maxWord = mathPow(2, 32);
+	var lengthProperty = 'length'
+	var i, j; // Used as a counter across the whole file
+	var result = ''
+
+	var words = [];
+	var asciiBitLength = ascii[lengthProperty]*8;
+	
+	//* caching results is optional - remove/add slash from front of this line to toggle
+	// Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
+	// (we actually calculate the first 64, but extra values are just ignored)
+	var hash = sha256.h = sha256.h || [];
+	// Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
+	var k = sha256.k = sha256.k || [];
+	var primeCounter = k[lengthProperty];
+	/*/
+	var hash = [], k = [];
+	var primeCounter = 0;
+	//*/
+
+	var isComposite = {};
+	for (var candidate = 2; primeCounter < 64; candidate++) {
+		if (!isComposite[candidate]) {
+			for (i = 0; i < 313; i += candidate) {
+				isComposite[i] = candidate;
+			}
+			hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
+			k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
+		}
+	}
+	
+	ascii += '\x80' // Append Ƈ' bit (plus zero padding)
+	while (ascii[lengthProperty]%64 - 56) ascii += '\x00' // More zero padding
+	for (i = 0; i < ascii[lengthProperty]; i++) {
+		j = ascii.charCodeAt(i);
+		if (j>>8) return; // ASCII check: only accept characters in range 0-255
+		words[i>>2] |= j << ((3 - i)%4)*8;
+	}
+	words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
+	words[words[lengthProperty]] = (asciiBitLength)
+	
+	// process each chunk
+	for (j = 0; j < words[lengthProperty];) {
+		var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
+		var oldHash = hash;
+		// This is now the undefinedworking hash", often labelled as variables a...g
+		// (we have to truncate as well, otherwise extra entries at the end accumulate
+		hash = hash.slice(0, 8);
+		
+		for (i = 0; i < 64; i++) {
+			var i2 = i + j;
+			// Expand the message into 64 words
+			// Used below if 
+			var w15 = w[i - 15], w2 = w[i - 2];
+
+			// Iterate
+			var a = hash[0], e = hash[4];
+			var temp1 = hash[7]
+				+ (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
+				+ ((e&hash[5])^((~e)&hash[6])) // ch
+				+ k[i]
+				// Expand the message schedule if needed
+				+ (w[i] = (i < 16) ? w[i] : (
+						w[i - 16]
+						+ (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) // s0
+						+ w[i - 7]
+						+ (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) // s1
+					)|0
+				);
+			// This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
+			var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
+				+ ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); // maj
+			
+			hash = [(temp1 + temp2)|0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
+			hash[4] = (hash[4] + temp1)|0;
+		}
+		
+		for (i = 0; i < 8; i++) {
+			hash[i] = (hash[i] + oldHash[i])|0;
+		}
+	}
+	
+	for (i = 0; i < 8; i++) {
+		for (j = 3; j + 1; j--) {
+			var b = (hash[i]>>(j*8))&255;
+			result += ((b < 16) ? 0 : '') + b.toString(16);
+		}
+	}
+	return result;
+};
+
