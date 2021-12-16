d5be294380af4af8b16494b77ab2aec17bcc5a54
David Melsheimer <dave.melshman@gmail.com>
2021-12-16 10:29:47 -0500

    ok, not winning with the JS dom manipulations of adding svg elements

---
+++ SVG_City_Kritters/active_folder/scene.js
@@ -0,0 +1,47 @@
+const container = document.querySelector('svg');
+
+let svgLinkNs = 'http://www.w3.org/1999/xlink';
+let svgNs = 'http://www.w3.org/2000/svg';
+
+//just have one instance on the DOM and make copies of it, resetting 
+//the needed attributes and resolving any clashing ids
+
+const theCopy = (index) => {
+    let copyMe = document.getElementById('whole_figure');
+    let theCopy = copyMe.cloneNode(true);
+    theCopy.setAttribute('id', `whole_figure_${index}`);
+    theCopy.setAttributeNS(svgLinkNs, 'viewBox', '-1000 0 3024 4032');
+    console.log(theCopy);
+    return theCopy;
+}
+
+function generateCopies(numCopies){
+    //numCopies between yatta and yadda
+    let copyMe = document.getElementById('whole_figure');
+
+
+    for(let i = 0; i < numCopies; i++){
+        let theCopy = copyMe.cloneNode(true);
+        container.appendChild(newCopy);
+        theCopy.setAttribute('id', `whole_figure_${index}`);
+        theCopy.setAttributeNS(svgLinkNs, 'viewBox', '-1000 0 3024 4032');
+    }
+}   
+
+generateCopies(4);
+
+let use = document.createElementNS(svgNs, 'use');
+document.getElementById('use_symbol').appendChild(use);
+use.setAttributeNS('href', '#whole_figure_0');
+use.setAttribute('transform', 'scale(4)');
+use.setAttribute('fill', 'red');
+
+
+
+
+use = document.createElementNS(svgNs, 'use');
+document.getElementById('use_symbol').appendChild(use);
+use.setAttributeNS('href', '#whole_figure_1');
+
+
+
