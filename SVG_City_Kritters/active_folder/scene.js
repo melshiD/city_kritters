d350c53eb92afb0524af2af4bb3e1875c58c1d26
David Melsheimer <dave.melshman@gmail.com>
2021-12-16 10:36:47 -0500

    ok, hallelujah.  It's been a namespace issue.  setAttrbuteNS

--- SVG_City_Kritters/active_folder/scene.js
+++ SVG_City_Kritters/active_folder/scene.js
@@ -6,14 +6,14 @@ let svgNs = 'http://www.w3.org/2000/svg';
 //just have one instance on the DOM and make copies of it, resetting 
 //the needed attributes and resolving any clashing ids
 
-const theCopy = (index) => {
-    let copyMe = document.getElementById('whole_figure');
-    let theCopy = copyMe.cloneNode(true);
-    theCopy.setAttribute('id', `whole_figure_${index}`);
-    theCopy.setAttributeNS(svgLinkNs, 'viewBox', '-1000 0 3024 4032');
-    console.log(theCopy);
-    return theCopy;
-}
+// const theCopy = (index) => {
+//     let copyMe = document.getElementById('whole_figure');
+//     let theCopy = copyMe.cloneNode(true);
+//     theCopy.setAttribute('id', `whole_figure_${index}`);
+//     theCopy.setAttributeNS(svgLinkNs, 'viewBox', '-1000 0 3024 4032');
+//     console.log(theCopy);
+//     return theCopy;
+// }
 
 function generateCopies(numCopies){
     //numCopies between yatta and yadda
@@ -22,9 +22,9 @@ function generateCopies(numCopies){
 
     for(let i = 0; i < numCopies; i++){
         let theCopy = copyMe.cloneNode(true);
-        container.appendChild(newCopy);
-        theCopy.setAttribute('id', `whole_figure_${index}`);
-        theCopy.setAttributeNS(svgLinkNs, 'viewBox', '-1000 0 3024 4032');
+        container.appendChild(theCopy);
+        theCopy.setAttribute('id', `whole_figure_${i}`);
+        theCopy.setAttribute('viewBox', '-1000 0 3024 4032');
     }
 }   
 
@@ -32,7 +32,7 @@ generateCopies(4);
 
 let use = document.createElementNS(svgNs, 'use');
 document.getElementById('use_symbol').appendChild(use);
-use.setAttributeNS('href', '#whole_figure_0');
+use.setAttributeNS(svgLinkNs, 'href', '#whole_figure_0');
 use.setAttribute('transform', 'scale(4)');
 use.setAttribute('fill', 'red');
 
@@ -41,7 +41,7 @@ use.setAttribute('fill', 'red');
 
 use = document.createElementNS(svgNs, 'use');
 document.getElementById('use_symbol').appendChild(use);
-use.setAttributeNS('href', '#whole_figure_1');
+use.setAttributeNS(svgLinkNs, 'href', '#whole_figure_1');
 
 
 
